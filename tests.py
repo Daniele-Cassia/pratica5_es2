307ac33ca9f77eca40e1c9f67d632efeda7e59fb
Daniele Cassia <danielecassia9@gmail.com>
2023-11-20 17:54:32 -0300

    testes

---
+++ tests.py
@@ -0,0 +1,27 @@
+# tests.py
+import unittest
+from program import calcular_media
+
+class TestCalcularMedia(unittest.TestCase):
+
+    def test_media_lista_inteiros(self):
+        numeros = [1, 2, 3, 4, 5]
+        resultado = calcular_media(numeros)
+        self.assertEqual(resultado, 3.0)
+
+    def test_media_lista_floats(self):
+        numeros = [1.5, 2.5, 3.5, 4.5, 5.5]
+        resultado = calcular_media(numeros)
+        self.assertEqual(resultado, 3.0)
+
+    def test_media_lista_mista(self):
+        numeros = [1, 2.5, 3, 4.5, 5]
+        resultado = calcular_media(numeros)
+        self.assertEqual(resultado, 3.0)
+
+    def test_media_lista_vazia(self):
+        with self.assertRaises(ValueError):
+            calcular_media([])
+
+if __name__ == '__main__':
+    unittest.main()
